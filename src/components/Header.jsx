34f095f1db89b73189bc368f03eab4907e3ed5f4
ravipatelctf <ravipatelctf@gmail.com>
2025-08-19 19:59:12 +0530

    feat: a nav item gets highlighted if its route matches the current pathname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import useProductContext from "../contexts/ProductContext";
-import { useState } from "react";
+import {useLocation} from "react-router-dom";
 
 export default function Header() {
     
     const {noOfUniqueProductsInCart, noOfProductsInWishlist, handleSearch} = useProductContext();
-
+    const {pathname} = useLocation();
+    console.log(pathname);
     return (
         <header className="text-secondary">
             <nav className="nav container px-4 py-2 align-items-center justify-content-between gap-1">
@@ -24,17 +25,17 @@ export default function Header() {
                 <ul className="navbar-nav flex-row gap-2">
 
                     <li className="nav-item">
-                        <Link to="/" className="nav-link">
+                        <Link to="/" className={`nav-link ${pathname === "/" ? "active fw-bold" : ""}`}>
                             Home
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/products" className="nav-link">
+                        <Link to="/products" className={`nav-link ${pathname === "/products" ? "active fw-bold" : ""}`}>
                             Products
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/wishlist" className="nav-link">
+                        <Link to="/wishlist" className={`nav-link ${pathname === "/wishlist" ? "active fw-bold" : ""}`}>
                         ♡
                         <sup className="fw-bold text-danger">
                             {noOfProductsInWishlist}
@@ -42,14 +43,14 @@ export default function Header() {
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/cart" className="nav-link">
+                        <Link to="/cart" className={`nav-link ${pathname === "/cart" ? "active fw-bold" : ""}`}>
                         🛒 Cart
                         <sup className="fw-bold text-danger">
                             {noOfUniqueProductsInCart}</sup>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/profile" className="nav-link">
+                        <Link to="/profile" className={`nav-link ${pathname === "/profile" ? "active fw-bold" : ""}`}>
                         👤 Profile
                         </Link>
                     </li>
