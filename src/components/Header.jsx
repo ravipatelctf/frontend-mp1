6335137536c8fd11108fb991c54f2ea8f096b7da
ravipatelctf <ravipatelctf@gmail.com>
2025-08-22 09:24:04 +0530

    chore: made the header component code DRY

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import {useLocation} from "react-router-dom";
 export default function Header() {
     
     const {noOfUniqueProductsInCart, noOfProductsInWishlist, handleSearch} = useProductContext();
-    const {pathname} = useLocation();
     return (
         <header className="text-secondary">
             <nav className="nav container px-4 py-2 align-items-center justify-content-between gap-1">
@@ -22,41 +21,31 @@ export default function Header() {
                     onChange={handleSearch}
                 />
                 <ul className="navbar-nav flex-row gap-3">
-
-                    <li className="nav-item">
-                        <Link to="/products" className={`nav-link ${pathname === "/products" ? "active fw-bold border-bottom border-primary border-5" : ""}`}>
-                            <h5>Products</h5>
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/wishlist" className={`nav-link ${pathname === "/wishlist" ? "active fw-bold border-bottom border-primary border-5" : ""}`}>
-                        <h4>
-                            ♡
-                            <sup className="fw-bold text-danger">
-                                {noOfProductsInWishlist}
-                            </sup>
-                        </h4>
-                        
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/cart" className={`nav-link ${pathname === "/cart" ? "active fw-bold border-bottom border-primary border-5" : ""}`}>
-                        <h5>
-                            🛒
-                            <sup className="fw-bold text-danger">
-                                {noOfUniqueProductsInCart}
-                            </sup>
-                        </h5>
-                        
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/profile" className={`nav-link ${pathname === "/profile" ? "active fw-bold border-bottom border-primary border-5" : ""}`}>
+                    <NavItem uri="/products">
+                        <h5>Products</h5>
+                    </NavItem>
+                    <NavItem uri="/wishlist">
+                        <h4>♡<sup className="fw-bold text-danger">{noOfProductsInWishlist}</sup></h4>
+                    </NavItem>
+                    <NavItem uri="/cart">
+                        <h5>🛒<sup className="fw-bold text-danger">{noOfUniqueProductsInCart}</sup></h5>
+                    </NavItem>
+                    <NavItem uri="/profile">
                         <h5>👤</h5>
-                        </Link>
-                    </li>
+                    </NavItem>
                 </ul>
             </nav>
         </header>
+    );
+}
+
+function NavItem({uri, children}) {
+    const {pathname} = useLocation();
+    return (
+        <li className="nav-item">
+            <Link to={uri} className={`nav-link ${pathname === uri ? "active fw-bold border-bottom border-primary border-5" : ""}`}>
+                {children}
+            </Link>
+        </li>
     );
 }
